e5f728631acdbf9f3237ddae308fbb96a69a3259
lagarcia <[leandrocanela279@gmail.com]>
2026-03-09 23:53:39 +0100

    probando useState inicial de input del usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
+  const [prompt, setPrompt] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setPrompt(inputValue);
+  };
   return (
     <main className="grid min-h-screen place-items-center bg-[radial-gradient(circle_at_top,rgba(234,179,8,0.22),transparent_30%),linear-gradient(160deg,#111827_0%,#172033_55%,#0f172a_100%)] px-6 py-10 text-stone-100">
       <section className="w-full max-w-2xl rounded-[28px] border border-white/12 bg-slate-950/70 p-8 shadow-[0_24px_80px_rgba(15,23,42,0.38)] backdrop-blur-xl sm:p-10">
@@ -20,13 +32,18 @@ export default function App() {
               id="barcelona-time"
               className="w-full rounded-[18px] border border-white/15 bg-white/6 px-5 py-4 text-base text-white outline-none transition duration-150 placeholder:text-white/40 hover:bg-white/8 focus:-translate-y-px focus:border-amber-300/75 focus:bg-white/10 focus:ring-4 focus:ring-amber-300/20"
               type="text"
+              onChange={handleInput}
               placeholder="Consulta el tiempo en Barcelona..."
             />
-            <button className=" rounded-full bg-amber-400 px-4 py-2 cursor-pointer text-black font-bold">
+            <button
+              className=" rounded-full bg-amber-400 px-4 py-2 cursor-pointer text-black font-bold"
+              onClick={handleSubmit}
+            >
               Enviar
             </button>
           </div>
         </label>
+        <p>{prompt}</p>
       </section>
     </main>
   );
